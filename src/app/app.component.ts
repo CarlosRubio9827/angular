e05bd8bbaeaa027355ccaab2c13bafd2b28e7cef
CarlosRubio9827 <43046210+CarlosRubio9827@users.noreply.github.com>
2019-04-14 23:29:24 -0500

    Cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular2';
+  users = ['Carlos', 'Laura', 'juan', "sdsdsd","dsdsd"];
 }
